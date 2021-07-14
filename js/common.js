a91bd1244b638446aa272483622189dfdf509c79
Roman <labunskij1997@gmail.com>
2021-07-14 19:09:39 +0300

    added new pages

--- js/common.js
+++ js/common.js
@@ -56,6 +56,14 @@ $('.slider-big').slick({
   dots: true,
   fade: true,
   asNavFor: ".slider-small",
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+      }
+    }
+  ]
 });
 
 
