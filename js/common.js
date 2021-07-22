3e6fca878aaea0bd8d658d24b5399ae6f4d8d7b1
Roman <labunskij1997@gmail.com>
2021-07-22 19:12:42 +0300

    fix some components positions added new sections

--- js/common.js
+++ js/common.js
@@ -1,33 +1,29 @@
-/*
-const menuBtn = document.querySelector(".menu-btn");
+$('.header_black').addClass('original').clone().insertAfter('header').addClass('cloned').css('position','fixed').css('top','0').css('margin-top','0').css('z-index','500').removeClass('original').hide();
 
-let menuOpen = false;
-menuBtn.addEventListener("click", () => {
-  document.getElementById("myDropdown").classList.toggle("show");
-  if(!menuOpen) {
-    menuBtn.classList.add("open");
-    menuOpen = true; 
+scrollIntervalID = setInterval(stickIt, 10);
+
+
+function stickIt() {
+
+  var orgElementPos = $('.original').offset();
+  orgElementTop = orgElementPos.top;               
+
+  if ($(window).scrollTop() >= (orgElementTop)) {
+    // scrolled past the original position; now only show the cloned, sticky element.
+
+    // Cloned element should always have same left position and width as original element.     
+    orgElement = $('.original');
+    coordsOrgElement = orgElement.offset();
+    leftOrgElement = coordsOrgElement.left;  
+    widthOrgElement = orgElement.css('width');
+    $('.cloned').css('left',leftOrgElement+'px').css('top',0).css('width',widthOrgElement).show();
+    $('.original').css('visibility','hidden');
   } else {
-    menuBtn.classList.remove("open");
-    menuOpen = false;
+    // not scrolled past the menu; only show the original menu.
+    $('.cloned').hide();
+    $('.original').css('visibility','visible');
   }
-
-  window.onclick = function(event) {
-    if (!event.target.matches('.menu-btn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-          menuBtn.classList.remove("open");
-          menuOpen = false;
-        }
-      }
-    }
-  }
-});
-*/
+}
 
 
 
