41e458adeaaf5d480b7e14ec8e212e57d3a54291
Roman <labunskij1997@gmail.com>
2021-07-29 13:44:27 +0300

    done some pages + fix

--- js/toUp.js
+++ js/toUp.js
@@ -28,6 +28,36 @@ $(function(){
       padding: 0,
       scrolling: 'auto'
     });
+
+    $('.fancybox-media').fancybox({
+      openEffect : 'none',
+      closeEffect : 'none',
+      width : 1280,
+      height : 720,
+      maxWidth : '100%',
+      maxHeight : '100%',
+      padding : 0,
+      margin : 0,
+      helpers : {
+      media : {
+      youtube : {
+      params : {
+      theme : 'light',
+      vq : 'hd720',
+      css : {
+      'body' : 'color: #fff'
+      }
+      }
+      }
+      }
+      }
+      });
+      
+      var hash = location.hash;
+      
+      if(hash == '#autoplay'){
+      $('#tinymce').find('.fancybox-media').trigger('click');
+      }
     
     $("#toTop").scrollToTop();
 
